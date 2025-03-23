3dc91deb83b7e936b6360af24b8a2b7f492560e9
ZorAnderius <gabriel.zorander@gmail.com>
2025-03-23 18:50:19 +0000

    add: axios api configuration

---
+++ src/api/configApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "https://foodie-node-api.onrender.com/api",
+});
+
+export const setToken = (token) => {
+  api.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+export const clearToken = () => {
+  api.defaults.headers.common.Authorization = "";
+};
