16b644914cd1c7a7d0faea38f682b4aa7267530f
ZorAnderius <gabriel.zorander@gmail.com>
2025-03-24 22:54:55 +0000

    add: render URL as a base axios URL

--- src/api/configApi.js
+++ src/api/configApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  // baseURL: "http://localhost:3000/api",
+  baseURL: "https://foodie-node-api.onrender.com/api",
 });
 
 export const setToken = (token) => {
