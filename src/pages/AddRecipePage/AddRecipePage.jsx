5b76f0013c473673905d86d0ef7a528e0a267f3d
dariia_babii <dariia.babii02@gmail.com>
2025-03-27 11:56:46 +0100

    PathInfo component and render AddRecipePage

---
+++ src/pages/AddRecipePage/AddRecipePage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import PathInfo from "../../components/PathInfo/PathInfo";
+import PageTitle from "../../components/PageTitle/PageTitle";
+import styles from "./AddRecipePage.module.css";
+
+const pageTitle = {
+  title: "Add recipe",
+  subtitle:
+    "Reveal your culinary art, share your favorite recipe and create gastronomic masterpieces with us.",
+};
+
+const AddRecipePage = () => {
+  //   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  //   if (!isAuthenticated) {
+  //     return <Navigate to="/login" />;
+  //   }
+
+  const pages = [
+    { path: "/", name: "Home" },
+    { path: "/recipe/add", name: "Add Recipe" },
+  ];
+
+  return (
+    <div className={styles.container}>
+      <PathInfo pages={pages} />
+      <PageTitle {...pageTitle} />
+      {/* <AddRecipeForm /> */}
+    </div>
+  );
+};
+
+export default AddRecipePage;
