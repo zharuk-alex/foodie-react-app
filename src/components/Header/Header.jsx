351ad5e83d6d8054eac73e0dcc1c3a093f863489
dfinchenko <dfinchenko@gmail.com>
2025-03-28 09:58:05 +0100

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,23 @@
-import Logo from "components/Logo/Logo";
-import NavMenu from "components/NavMenu/NavMenu";
-import css from "./Header.module.css";
-
-import { clsx } from "clsx";
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import clsx from 'clsx';
+import Logo from '../Logo/Logo';
+import css from './Header.module.css';
+import NavMenu from '../NavMenu/NavMenu';
 
 const Header = () => {
+  const location = useLocation();
+  const isInverse = location.pathname === '/';
+
   return (
-    <header className={css.header}>
-      <div className={clsx("container", css.wrapper)}>
-        <Logo className={css.logo} />
-        <NavMenu></NavMenu>
+    <header>
+      <div className={css.headerContainer}>
+        <div
+          className={clsx(css.header, isInverse)}
+        >
+          <Logo />
+          <NavMenu isInverse={isInverse} />
+        </div>
       </div>
     </header>
   );
