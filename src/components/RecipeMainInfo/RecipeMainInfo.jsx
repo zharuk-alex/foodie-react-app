76a56df280a3eaab9f797ec9d2629b0ef2b61705
tufankaracam <tufankaracam@hotmail.com.tr>
2025-03-28 16:14:01 +0100

    recipe owner login status navigate implementation | login logout refresh recipe & popular recipes fix

--- src/components/RecipeMainInfo/RecipeMainInfo.jsx
+++ src/components/RecipeMainInfo/RecipeMainInfo.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import css from "./RecipeMainInfo.module.css";
-export default function RecipeMainInfo({ recipe }) {
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setModalLoginOpen } from "../../store/modal/operations";
+export default function RecipeMainInfo({ recipe, isLoggedIn }) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleClick = () => {
+    if (isLoggedIn) {
+      navigate(`/user/${recipe?.ownerId}`);
+    } else {
+      dispatch(setModalLoginOpen(true));
+    }
+  };
   return (
     <div className={css.card}>
       <h2 className={css.title}>{recipe?.title}</h2>
@@ -9,13 +21,19 @@ export default function RecipeMainInfo({ recipe }) {
         <span className={css.info}>{`${recipe?.time} min`}</span>
       </div>
       <p className={css.text}>{recipe?.description}</p>
-      <div className={css.userContainer}>
-        <img className={css.userAvatar} src={`${recipe?.ownerAvatar || 'https://placehold.co/32x32'}`} alt="" />
+      <button className={css.userContainer} onClick={handleClick}>
+        <img
+          className={css.userAvatar}
+          src={`${
+            recipe?.ownerAvatar || "/src/images/avatar/default_avatar.jpg"
+          }`}
+          alt=""
+        />
         <div className={css.userDetails}>
           <span className={css.userDetailName}>Created by:</span>
           <span className={css.userDetailValue}>{recipe?.ownerName}</span>
         </div>
-      </div>
+      </button>
     </div>
   );
 }
