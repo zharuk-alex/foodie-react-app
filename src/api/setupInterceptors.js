3c68ea418e56796a889bc7ae9f9e06652bf041c4
zharuk.alex <zzharuk@gmail.com>
2025-03-29 16:38:32 +0200

    setupInterceptors fixed

--- src/api/setupInterceptors.js
+++ src/api/setupInterceptors.js
@@ -2,14 +2,17 @@ import { api } from './configApi';
 import { forceLogout } from '../store/auth/slice';
 import toast from 'react-hot-toast';
 
-export const setupInterceptors = (store) => {
+export const setupInterceptors = store => {
   api.interceptors.response.use(
     response => response,
     error => {
-      if (error.response?.status === 401) {
+      const hasToken = !!api.defaults.headers.common['Authorization'];
+
+      if (error.response?.status === 401 && hasToken) {
         store.dispatch(forceLogout());
         toast.error('Session expired');
       }
+
       return Promise.reject(error);
     }
   );
