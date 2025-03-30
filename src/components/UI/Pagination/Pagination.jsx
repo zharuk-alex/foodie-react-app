67ec105d3f8c8b3b4a1bc8124f986989acf0a7f4
ZorAnderius <gabriel.zorander@gmail.com>
2025-03-30 16:48:20 +0100

    update: pagination and popular recipes user avatar and name

--- src/components/UI/Pagination/Pagination.jsx
+++ src/components/UI/Pagination/Pagination.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import css from "./Pagination.module.css";
 import clsx from "clsx";
 
-const Pagination = ({ total = 3, current = 1, onChange = () => {} }) => {
-  const pages = Array.from({ length: total }, (_, i) => i + 1);
-
+const Pagination = ({ total = 3, current = 1, onChange = () => {},  }) => {
+  const getPagesToShow = () => {
+    if (total <= 3) {
+      return Array.from({ length: total }, (_, i) => i + 1);
+    }
+    if (current <= 2) return [1, 2, 3];
+    if (current >= total - 1) return [total - 2, total - 1, total];
+    return [current - 1, current, current + 1];
+  };
   return (
     <div className={css.pagination}>
-      {pages.map((page) => (
+      {getPagesToShow().map(page => (
         <button
           key={page}
+          onClick={() => onChange(page)}
           className={clsx(css.pageBtn, {
             [css.active]: page === current,
           })}
-          onClick={() => onChange(page)}
+          disabled={current === page}
         >
           {page}
         </button>
